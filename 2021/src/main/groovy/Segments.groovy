261e06d036573e25f8cdd145adfd956d24432335
Kirill Artamonov <artamonov.kirill@gmail.com>
2021-12-08 09:58:31 +0100

    day 8 solution refactored

--- 2021/src/main/groovy/Segments.groovy
+++ 2021/src/main/groovy/Segments.groovy
@@ -1,36 +1,40 @@
 class Segments {
-    static List<Integer> decode(List<String> data) {
-        data.collect { line ->
-            def io = line.split('[|]')
-            def input = io[0].trim().split(' ')
-            def output = io[1].trim().split(' ')
-            def all = input + output
+    static List<Integer> decode(List<String> displays) {
+        displays.collect { display ->
+            def io = display.split('[|]').collect { it.trim().split(' ') }
+            def output = io[1]
+            def all = io[0] + output
             def one = all.find { it.size() == 2 }
             def four = all.find { it.size() == 4 }
             if (!one || !four) throw new RuntimeException("Need one and four to decode")
-            output.collect {
-                d -> parse(d, one, four)
-            }.join('') as int
+            output.collect { digit -> parse(digit, one, four) }.join('') as int
         }
     }
 
-    private static String parse(String d, String one, String four) {
-        if (d.size() == 2) return 1
-        if (d.size() == 4) return 4
-        if (d.size() == 3) return 7
-        if (d.size() == 7) return 8
-        if (d.size() == 5) {
-            return d.toList().containsAll(one.toList())
-                    ? 3
-                    : d.toList().containsAll(four.toList() - one.toList()) ? 5 : 2
+    private static String parse(String digit, String one, String four) {
+        switch (digit.size()) {
+            case 2: return 1
+            case 4: return 4
+            case 3: return 7
+            case 7: return 8
+            case 5:
+                return contains(digit, one)
+                        ? 3
+                        : contains(digit, four.toList() - one.toList()) ? 5 : 2
+            case 6:
+                return contains(digit, one)
+                        ? (contains(digit, four) ? 9 : 0)
+                        : 6
+            default: digit
+        }
+    }
 
-        }
-        if (d.size() == 6) {
-            return d.toList().containsAll(one.toList())
-                    ? (d.toList().containsAll(four.toList()) ? 9 : 0)
-                    : 6
-        }
-        return d
+    private static boolean contains(String a, String b) {
+        contains(a, b.toList())
+    }
+
+    private static boolean contains(String a, List<String> b) {
+        a.toList().containsAll(b)
     }
 
     static void main(String... args) {
