22bf0d50ad6d355182ad2dcea6d2524c6de1cba1
Kirill Artamonov <artamonov.kirill@gmail.com>
2023-12-13 23:41:45 +0100

    :sparkles: 2023 day 12 part 2 (WIP)

--- 2023/12/main.go
+++ 2023/12/main.go
@@ -3,75 +3,153 @@ package main
 import (
 	"fmt"
 	"github.com/advendofcode/util"
-	"math"
 	"regexp"
-	"strconv"
 	"strings"
 )
 
-func Solve(file string) int {
+type candidate struct {
+	value  string
+	groups int
+}
+
+func Solve(file string, factor int) int {
 	result := 0
-	for _, line := range util.Lines(file) {
-		parts := strings.Split(line, " ")
-		visual := unfold(parts[0], "?")
-		r := regexp.MustCompile(rx(unfold(parts[1], ",")))
-		unknowns := strings.Count(visual, "?")
-		matches := 0
-		for i := 0; i < int(math.Pow(2, float64(unknowns))); i++ {
-			b := binary(i, unknowns)
-			j := 0
-			var candidate []int32
-			for _, c := range visual {
-				if c == '?' {
-					x := b[j]
-					if x == '0' {
-						candidate = append(candidate, '.')
-					} else {
-						candidate = append(candidate, '#')
-					}
-					j++
-				} else {
-					candidate = append(candidate, c)
-				}
-			}
-			if r.MatchString(string(candidate)) {
-				matches++
-			}
-		}
-		result += matches
+	for i, line := range util.Lines(file) {
+		fmt.Println("processing", i)
+		result += Matches(line, factor)
 	}
 	return result
 }
 
-func unfold(pattern, separator string) string {
-	result := make([]string, 1)
-	for i := 0; i < 1; i++ {
+type hash struct {
+	groups, length int
+}
+
+func Matches(line string, factor int) int {
+	result := 0
+	parts := strings.Split(line, " ")
+	visual := regex(unfold(parts[0], "?", factor))
+	groups := parse(unfold(parts[1], ",", factor))
+
+	current := []candidate{
+		{},
+	}
+	var rvs []*regexp.Regexp
+	for i := 0; i < len(visual); i++ {
+		rvs = append(rvs, regexp.MustCompile(visual[0:i]))
+	}
+	rts := make(map[int]*regexp.Regexp)
+	for i := 0; i < len(visual); i++ {
+		v := visual[i:]
+		rts[len(v)] = regexp.MustCompile(v)
+	}
+
+	for len(current) > 0 {
+		var next []candidate
+		mc := make(map[hash]int)
+		for _, c := range current {
+			remainingGroups := len(groups) - c.groups
+
+			var ns []string
+			if remainingGroups == 0 {
+				ns = []string{"x"}
+			} else if len(c.value) > 0 && c.value[len(c.value)-1] == '#' {
+				ns = []string{"x"}
+			} else {
+				switch visual[len(c.value)] {
+				case 'x':
+					ns = []string{"x"}
+				case '#':
+					ns = []string{"#"}
+				case '.':
+					ns = []string{"x", "#"}
+				default:
+					panic("unexpected first char")
+				}
+			}
+
+			for _, char := range ns {
+				var addition string
+				var nextGroups []int
+
+				if char == "#" && remainingGroups > 0 {
+					nextGroup := groups[c.groups]
+					nextGroups = groups[0 : c.groups+1]
+					addition = strings.Repeat(char, nextGroup)
+					if remainingGroups > 1 {
+						addition += "x"
+					}
+				} else {
+					nextGroups = groups[0:c.groups]
+					addition = char
+				}
+
+				if len(addition)+len(c.value) == len(visual) {
+					if rts[len(addition)].MatchString(addition) && equal(nextGroups, groups) {
+						result++
+					}
+				} else {
+					value := c.value + addition
+					if len(value) < len(visual) {
+						nc := candidate{
+							value:  value,
+							groups: len(nextGroups),
+						}
+						if rvs[len(nc.value)].MatchString(nc.value) {
+							h := hash{nc.groups, len(nc.value)}
+							mc[h] = mc[h] + 1
+							next = append(next, nc)
+						}
+					}
+				}
+			}
+		}
+		if len(next) > 100_000 {
+			fmt.Println("  ", len(next), "to go,", "unique", len(mc))
+		}
+		current = next
+	}
+
+	return result
+}
+
+func equal(as []int, bs []int) bool {
+	if len(as) != len(bs) {
+		return false
+	}
+	for i, a := range as {
+		if bs[i] != a {
+			return false
+		}
+	}
+	return true
+}
+
+func parse(input string) []int {
+	var result []int
+	for _, n := range strings.Split(input, ",") {
+		result = append(result, util.Number(n))
+	}
+	return result
+}
+
+func regex(input string) string {
+	return strings.ReplaceAll(
+		strings.ReplaceAll(
+			input,
+			".", "x"),
+		"?", ".")
+}
+
+func unfold(pattern, separator string, factor int) string {
+	result := make([]string, factor)
+	for i := 0; i < factor; i++ {
 		result[i] = pattern
 	}
 	return strings.Join(result, separator)
 }
 
-func rx(counts string) string {
-	result := "^\\.*"
-	cs := strings.Split(counts, ",")
-	for i, count := range cs {
-		result += "#{" + count + "}"
-		if i < len(cs)-1 {
-			result += "\\.+"
-		}
-	}
-	result += "\\.*$"
-	return result
-}
-
-func binary(i, length int) string {
-	result := strconv.FormatInt(int64(i), 2)
-	for len(result) < length {
-		result = "0" + result
-	}
-	return result
-}
-
 func main() {
-	fmt.Println(Solve("2023/12/input.txt"))
+	fmt.Println(Solve("2023/12/input.txt", 1))
+	fmt.Println(Solve("2023/12/input.txt", 5))
 }
--- 2023/12/main_test.go
+++ 2023/12/main_test.go
@@ -7,5 +7,33 @@ import (
 
 func TestSolve(t *testing.T) {
 	// expect:
-	assert.Equal(t, Solve("example.txt"), 21)
+	assert.Equal(t, Solve("example.txt", 1), 21)
+	assert.Equal(t, Solve("example.txt", 5), 525152)
+}
+
+func TestSolveOne(t *testing.T) {
+	for _, tc := range []struct {
+		input    string
+		factor   int
+		expected int
+	}{
+		{"???.### 1,1,3", 1, 1},
+		{".??..??...?##. 1,1,3", 1, 4},
+		{"?#?#?#?#?#?#?#? 1,3,1,6", 1, 1},
+		{"????.#...#... 4,1,1", 1, 1},
+		{"????.######..#####. 1,6,5", 1, 4},
+		{"?###???????? 3,2,1", 1, 10},
+
+		{"???.### 1,1,3", 5, 1},
+		{".??..??...?##. 1,1,3", 5, 16384},
+		{"?#?#?#?#?#?#?#? 1,3,1,6", 5, 1},
+		{"????.#...#... 4,1,1", 5, 16},
+		{"????.######..#####. 1,6,5", 5, 2500},
+		{"?###???????? 3,2,1", 5, 506250},
+	} {
+		t.Run(tc.input, func(t *testing.T) {
+			// expect:
+			assert.Equal(t, Matches(tc.input, tc.factor), tc.expected)
+		})
+	}
 }
