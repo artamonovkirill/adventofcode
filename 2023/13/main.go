381f52914377d7bf02a4e515c91d6a4a5b29e7a1
Kirill Artamonov <artamonov.kirill@gmail.com>
2023-12-13 15:27:31 +0100

    :sparkles: 2023 day 13 part 2

--- 2023/13/main.go
+++ 2023/13/main.go
@@ -10,54 +10,107 @@ func Solve(file string) int {
 	result := 0
 
 	patterns := strings.Split(util.Text(file), "\n\n")
+p:
 	for _, pattern := range patterns {
-		lines := strings.Split(pattern, "\n")
-	outer:
-		for i := 1; i < len(lines); i++ {
-			top := lines[0:i]
-			bottom := lines[i:]
-			length := min(len(bottom), len(top))
-			bottom = bottom[0:length]
-			for j := 0; j < length; j++ {
-				if bottom[j] != top[len(top)-j-1] {
-					continue outer
-				}
-			}
-			result += 100 * len(top)
-		}
+		matrix := parse(pattern)
+		originalH := firstOr(reflection(matrix), 0)
+		originalV := firstOr(reflection(transpose(matrix)), 0)
 
-		columns := transpose(strings.Split(pattern, "\n"))
-	outer2:
-		for i := 1; i < len(columns); i++ {
-			top := columns[0:i]
-			bottom := columns[i:]
-			length := min(len(bottom), len(top))
-			bottom = bottom[0:length]
-			for j := 0; j < length; j++ {
-				if bottom[j] != top[len(top)-j-1] {
-					continue outer2
+		for y, line := range matrix {
+			for x, c := range line {
+				if c == '#' {
+					matrix[y][x] = '.'
+				} else {
+					matrix[y][x] = '#'
+				}
+
+				hrz := reflection(matrix)
+				vrt := reflection(transpose(matrix))
+				if len(hrz) > 0 || len(vrt) > 0 {
+					addition := 0
+					for _, h := range hrz {
+						if h != originalH {
+							addition += 100 * h
+						}
+					}
+					for _, v := range vrt {
+						if v != originalV {
+							addition += v
+						}
+					}
+					if addition > 0 {
+						result += addition
+						continue p
+					}
+				}
+				matrix[y][x] = c
+				if toString(matrix) != pattern {
+					panic("drift detected")
 				}
 			}
-			result += len(top)
 		}
+		panic("no smudge found")
 	}
 
 	return result
 }
 
-func transpose(lines []string) []string {
-	transposed := make([][]int32, len(lines[0]))
-	for i := 0; i < len(lines[0]); i++ {
-		transposed[i] = make([]int32, len(lines))
+func firstOr(matches []int, def int) int {
+	if len(matches) == 0 {
+		return def
 	}
+	return matches[0]
+}
+
+func toString(m [][]int32) string {
+	lines := make([]string, len(m))
+	for i, l := range m {
+		lines[i] = string(l)
+	}
+	return strings.Join(lines, "\n")
+}
+
+func reflection(matrix [][]int32) []int {
+	var matches []int
+outer:
+	for i := 1; i < len(matrix); i++ {
+		top := matrix[0:i]
+		bottom := matrix[i:]
+		length := min(len(bottom), len(top))
+		bottom = bottom[0:length]
+		for j := 0; j < length; j++ {
+			for x, c := range bottom[j] {
+				if c != top[len(top)-j-1][x] {
+					continue outer
+				}
+			}
+		}
+		matches = append(matches, len(top))
+	}
+	return matches
+}
+
+func parse(pattern string) [][]int32 {
+	lines := strings.Split(pattern, "\n")
+	result := make([][]int32, len(lines))
 	for y, line := range lines {
+		result[y] = make([]int32, len(line))
 		for x, c := range line {
-			transposed[x][y] = c
+			result[y][x] = c
 		}
 	}
-	result := make([]string, len(transposed))
-	for i, line := range transposed {
-		result[i] = string(line)
+	return result
+}
+
+func transpose(m [][]int32) [][]int32 {
+	result := make([][]int32, len(m[0]))
+	for i := 0; i < len(m[0]); i++ {
+		result[i] = make([]int32, len(m))
+	}
+	for y, line := range m {
+		for x, c := range line {
+			result[x][y] = c
+		}
 	}
 	return result
 }
--- 2023/13/main_test.go
+++ 2023/13/main_test.go
@@ -7,5 +7,5 @@ import (
 
 func TestSolve(t *testing.T) {
 	// expect:
-	assert.Equal(t, Solve("example.txt"), 405)
+	assert.Equal(t, Solve("example.txt"), 400)
 }
