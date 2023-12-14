bf0f5531284e3e4d8a78a612a39d2983981cabb6
Kirill Artamonov <artamonov.kirill@gmail.com>
2023-12-14 21:50:52 +0100

    :sparkles: 2023 day 14 part 2

--- 2023/14/main.go
+++ 2023/14/main.go
@@ -16,19 +16,53 @@ func (r *rock) isMovable() bool {
 }
 
 func Solve(file string) int {
-	lines := util.Lines(file)
-	m := make([][]*rock, len(lines))
-	for y, line := range lines {
-		m[y] = make([]*rock, len(line))
-		for x, c := range line {
-			if c == 'O' {
-				m[y][x] = &rock{x: x, y: y, movable: true}
-			} else if c == '#' {
-				m[y][x] = &rock{x: x, y: y}
+	m := parse(file)
+
+	n := 1_000_000_000
+	var ns []int
+	for i := 1; i <= n; i++ {
+		cycle(m)
+		intermediate := 0
+		for j, row := range m {
+			for _, r := range row {
+				if r != nil && r.isMovable() {
+					intermediate += len(m) - j
+				}
+			}
+		}
+		ns = append(ns, intermediate)
+		if len(ns) > 2 {
+		outer:
+			for j := 1; j < len(ns)/3; j++ {
+				tail := ns[len(ns)-j*3:]
+				one := tail[0:j]
+				two := tail[j : 2*j]
+				three := tail[2*j : 3*j]
+				for k, e := range one {
+					if e != two[k] || e != three[k] {
+						continue outer
+					}
+				}
+				rest := n - len(ns)
+				rest = rest % len(one)
+				return one[rest-1]
 			}
 		}
 	}
 
+	result := 0
+	for i, row := range m {
+		for _, r := range row {
+			if r != nil && r.isMovable() {
+				result += len(m) - i
+			}
+		}
+	}
+
+	return result
+}
+
+func tiltUp(m [][]*rock) {
 outer:
 	for {
 		for y := 1; y < len(m); y++ {
@@ -46,17 +80,91 @@ outer:
 		}
 		break outer
 	}
+}
 
-	result := 0
-	for i, row := range m {
-		for _, r := range row {
-			if r != nil && r.isMovable() {
-				result += len(m) - i
+func tiltDown(m [][]*rock) {
+outer:
+	for {
+		for y := 0; y < len(m)-1; y++ {
+			restart := false
+			for x, e := range m[y] {
+				if e != nil && e.isMovable() && m[y+1][x] == nil {
+					restart = true
+					m[y+1][x] = e
+					m[y][x] = nil
+				}
+			}
+			if restart {
+				continue outer
+			}
+		}
+		break outer
+	}
+}
+
+func tiltLeft(m [][]*rock) {
+outer:
+	for {
+		for x := 1; x < len(m[0]); x++ {
+			restart := false
+			for y := 0; y < len(m); y++ {
+				e := m[y][x]
+				if e != nil && e.isMovable() && m[y][x-1] == nil {
+					restart = true
+					m[y][x-1] = e
+					m[y][x] = nil
+				}
+			}
+			if restart {
+				continue outer
+			}
+		}
+		break outer
+	}
+}
+
+func tiltRight(m [][]*rock) {
+outer:
+	for {
+		for x := 0; x < len(m[0])-1; x++ {
+			restart := false
+			for y := 0; y < len(m); y++ {
+				e := m[y][x]
+				if e != nil && e.isMovable() && m[y][x+1] == nil {
+					restart = true
+					m[y][x+1] = e
+					m[y][x] = nil
+				}
+			}
+			if restart {
+				continue outer
+			}
+		}
+		break outer
+	}
+}
+
+func cycle(m [][]*rock) {
+	tiltUp(m)
+	tiltLeft(m)
+	tiltDown(m)
+	tiltRight(m)
+}
+
+func parse(file string) [][]*rock {
+	lines := util.Lines(file)
+	m := make([][]*rock, len(lines))
+	for y, line := range lines {
+		m[y] = make([]*rock, len(line))
+		for x, c := range line {
+			if c == 'O' {
+				m[y][x] = &rock{x: x, y: y, movable: true}
+			} else if c == '#' {
+				m[y][x] = &rock{x: x, y: y}
 			}
 		}
 	}
-
-	return result
+	return m
 }
 
 func toString(m [][]*rock) string {
--- 2023/14/main_test.go
+++ 2023/14/main_test.go
@@ -7,5 +7,39 @@ import (
 
 func TestSolve(t *testing.T) {
 	// expect:
-	assert.Equal(t, Solve("example.txt"), 136)
+	assert.Equal(t, Solve("example.txt"), 64)
+}
+
+func TestTilt(t *testing.T) {
+	// given:
+	m := parse("example.txt")
+
+	// when:
+	tiltUp(m)
+
+	// then:
+	assert.Equal(t, toString(m), "OOOO.#.O..\nOO..#....#\nOO..O##..O\nO..#.OO...\n........#.\n..#....#.#\n..O..#.O.O\n..O.......\n#....###..\n#....#....")
+}
+
+func TestCycle(t *testing.T) {
+	// given:
+	m := parse("example.txt")
+
+	// when:
+	cycle(m)
+
+	// then:
+	assert.Equal(t, toString(m), ".....#....\n....#...O#\n...OO##...\n.OO#......\n.....OOO#.\n.O#...O#.#\n....O#....\n......OOOO\n#...O###..\n#..OO#....")
+
+	// when:
+	cycle(m)
+
+	// then:
+	assert.Equal(t, toString(m), ".....#....\n....#...O#\n.....##...\n..O#......\n.....OOO#.\n.O#...O#.#\n....O#...O\n.......OOO\n#..OO###..\n#.OOO#...O")
+
+	// when:
+	cycle(m)
+
+	// then:
+	assert.Equal(t, toString(m), ".....#....\n....#...O#\n.....##...\n..O#......\n.....OOO#.\n.O#...O#.#\n....O#...O\n.......OOO\n#...O###.O\n#.OOO#...O")
 }
