21c4214a4e76e9aea49134a889ec9169ca447720
Kirill Artamonov <artamonov.kirill@gmail.com>
2024-01-02 17:38:06 +0100

    :sparkles:  2023 day 18 part 2
    Based on https://www.reddit.com/r/adventofcode/comments/18l0qtr/comment/kdveugr/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button

--- 2023/18/main.go
+++ 2023/18/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/advendofcode/util"
+	"strconv"
 	"strings"
 )
 
@@ -10,122 +11,92 @@ type point struct {
 	x, y int
 }
 
-func Solve(file string) int {
+func Solve(file string, parse func(file string) []point) int {
+	points := parse(file)
+
+	area := 0
+	perimeter := 0
+	for i := 0; i < len(points)-1; i++ {
+		area += points[i].x * points[i+1].y
+		area -= points[i+1].x * points[i].y
+		perimeter += (max(points[i+1].y, points[i].y)-min(points[i+1].y, points[i].y)+1)*(max(points[i+1].x, points[i].x)-min(points[i+1].x, points[i].x)+1) - 1
+	}
+
+	first := points[0]
+	last := points[len(points)-1]
+	area += last.x * first.y
+	area -= first.x * last.y
+	perimeter += (max(first.y, last.y)-min(first.y, last.y)+1)*(max(first.x, last.x)-min(first.x, last.x)+1) - 1
+	area = area / 2
+	inner := area - perimeter/2 + 1
+
+	return inner + perimeter
+}
+
+func parse(file string) []point {
 	x := 0
 	y := 0
-	border := []point{{x, y}}
-	for _, line := range util.Lines(file) {
-		parts := strings.Split(line, " ")
+	var points []point
+	for _, l := range util.Lines(file) {
+		parts := strings.Split(l, " ")
 		direction := parts[0]
 		steps := util.Number(parts[1])
 		switch direction {
 		case "R":
-			for i := 1; i <= steps; i++ {
-				x++
-				border = append(border, point{x, y})
-			}
+			points = append(points, point{x + steps, y})
+			x += steps
 		case "L":
-			for i := 1; i <= steps; i++ {
-				x--
-				border = append(border, point{x, y})
-			}
+			points = append(points, point{x - steps, y})
+			x -= steps
 		case "D":
-			for i := 1; i <= steps; i++ {
-				y++
-				border = append(border, point{x, y})
-			}
+			points = append(points, point{x, y + steps})
+			y += steps
 		case "U":
-			for i := 1; i <= steps; i++ {
-				y--
-				border = append(border, point{x, y})
-			}
+			points = append(points, point{x, y - steps})
+			y -= steps
 		default:
-			panic("not implemented for " + direction)
-		}
-	}
-
-	if border[len(border)-1] != border[0] {
-		panic("how?")
-	}
-	border = append([]point{border[len(border)-2]}, border...)
-
-	borderLine := make(map[point]string)
-	for i := 1; i < len(border)-1; i++ {
-		previous := border[i-1]
-		self := border[i]
-		next := border[i+1]
-		if _, ok := borderLine[self]; ok {
-			panic("already been here")
-		}
-		if previous.x == self.x {
-			if self.x == next.x {
-				borderLine[self] = "|"
-			} else if previous.y < self.y && self.x < next.x {
-				borderLine[self] = "L"
-			} else if previous.y < self.y && self.x > next.x {
-				borderLine[self] = "J"
-			} else if previous.y > self.y && self.x < next.x {
-				borderLine[self] = "F"
-			} else if previous.y > self.y && self.x > next.x {
-				borderLine[self] = "7"
-			}
-		} else if previous.y == self.y {
-			if self.y == next.y {
-				borderLine[self] = "-"
-			} else if previous.x > self.x && self.y > next.y {
-				borderLine[self] = "L"
-			} else if previous.x > self.x && self.y < next.y {
-				borderLine[self] = "F"
-			} else if previous.x < self.x && self.y < next.y {
-				borderLine[self] = "7"
-			} else if previous.x < self.x && self.y > next.y {
-				borderLine[self] = "J"
-			}
-		} else {
 			panic("not implemented")
 		}
 	}
+	return points
+}
 
-	minX := int(^uint(0) >> 1)
-	minY := int(^uint(0) >> 1)
-	maxX := 0
-	maxY := 0
-	for p := range borderLine {
-		minX = min(minX, p.x)
-		maxX = max(maxX, p.x)
-		minY = min(minY, p.y)
-		maxY = max(maxY, p.y)
-	}
-
-	result := len(borderLine)
-
-	for y := 0; y <= maxY-minY; y++ {
-		in := false
-		up := false
-		down := false
-		for x := 0; x <= maxX-minX; x++ {
-			e := borderLine[point{x + minX, y + minY}]
-			if e != "" {
-				if e == "F" || e == "7" || e == "|" {
-					down = !down
-				}
-				if e == "L" || e == "J" || e == "|" {
-					up = !up
-				}
-				if up && down {
-					in = !in
-					up = false
-					down = false
-				}
-			} else if in {
-				result++
-			}
+func parse2(file string) []point {
+	x := 0
+	y := 0
+	var points []point
+	for _, l := range util.Lines(file) {
+		instruction := strings.Split(l, " ")[2]
+		direction := instruction[len(instruction)-2]
+		sts, err := strconv.ParseUint(instruction[2:len(instruction)-2], 16, 62)
+		if err != nil {
+			panic(err)
+		}
+		steps := int(sts)
+		if uint64(steps) != sts {
+			panic("how?")
+		}
+		switch direction {
+		case '0': //R
+			points = append(points, point{x + steps, y})
+			x += steps
+		case '2': //L
+			points = append(points, point{x - steps, y})
+			x -= steps
+		case '1': //D
+			points = append(points, point{x, y + steps})
+			y += steps
+		case '3': //U
+			points = append(points, point{x, y - steps})
+			y -= steps
+		default:
+			panic("not implemented")
 		}
 	}
-
-	return result
+	return points
 }
 
 func main() {
-	fmt.Println(Solve("2023/18/input.txt"))
+	fmt.Println(Solve("2023/18/input.txt", parse))
+	fmt.Println(Solve("2023/18/input.txt", parse2))
 }
--- 2023/18/main_test.go
+++ 2023/18/main_test.go
@@ -7,5 +7,7 @@ import (
 
 func TestSolve(t *testing.T) {
 	// expect:
-	assert.Equal(t, Solve("example.txt"), 62)
+	assert.Equal(t, Solve("example.txt", parse), 62)
+	assert.Equal(t, Solve("input.txt", parse), 44436)
+	assert.Equal(t, Solve("example.txt", parse2), 952408144115)
 }
