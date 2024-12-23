83d6ca375799d887e3924e7417af03e94ffd883f
Kirill Artamonov <artamonov.kirill@gmail.com>
2024-12-23 19:24:01 +0100

    :sparkles: 2024 day 23 part 2

--- 2024/23/d23.py
+++ 2024/23/d23.py
@@ -30,5 +30,35 @@ def solve(file: str) -> int:
     return len(trios)
 
 
+def solve2(file: str) -> str:
+    path = Path(__file__).parent / file
+    with path.open() as f:
+        content = f.readlines()
+    connections = defaultdict(lambda: set())
+    for line in content:
+        connection = line.rstrip()
+        a, b = connection.split('-')
+        connections[a].add(b)
+        connections[b].add(a)
+
+    def expand(start: str) -> set[str]:
+        party = {start}
+        current = connections[start]
+        for c in current:
+            nxt = set()
+            if c not in party and all(c in connections[p] for p in party):
+                nxt.add(c)
+                party.add(c)
+        return party
+
+    best = set()
+    for node in connections:
+        candidate = expand(node)
+        if len(candidate) > len(best):
+            best = candidate
+    return ','.join(sorted(best))
+
+
 if __name__ == "__main__":
     print(solve('puzzle.txt'))
+    print(solve2('puzzle.txt'))
--- 2024/23/d23_test.py
+++ 2024/23/d23_test.py
@@ -1,5 +1,6 @@
-from d23 import solve
+from d23 import solve, solve2
 
 
 def test_solves_example():
     assert solve('example.txt') == 7
+    assert solve2('example.txt') == 'co,de,ka,ta'
