c23881f1bb916a2cf91fb090690d0a6e5a68286d
Kirill Artamonov <artamonov.kirill@gmail.com>
2025-12-11 23:51:20 +0100

    :sparkles: day 11, part 2

--- 2025/11/d11.py
+++ 2025/11/d11.py
@@ -25,5 +25,38 @@ def solve(file: str) -> int:
     return result
 
 
+def solve2(file: str) -> int:
+    path = Path(__file__).parent / file
+    with path.open() as f:
+        content = f.readlines()
+    links = defaultdict(lambda: [])
+    for line in content:
+        frm, tos = line.replace('\n', '').split(': ')
+        for to in tos.split(' '):
+            links[frm].append(to)
+
+    fft_dac = paths(links, 'svr', 'fft') * paths(links, 'fft', 'dac') * paths(links, 'dac', 'out')
+    dac_fft = paths(links, 'svr', 'dac') * paths(links, 'dac', 'fft') * paths(links, 'fft', 'out')
+    return fft_dac + dac_fft
+
+
+def paths(links: dict[str, list[str]], start: str, end: str):
+    result = 0
+    current = {start: 1}
+    while current:
+        nxt = dict()
+        for frm, count in current.items():
+            for to in links[frm]:
+                if to not in nxt:
+                    nxt[to] = count
+                else:
+                    nxt[to] += count
+        if end in nxt:
+            result += nxt[end]
+        current = nxt
+    return result
+
+
 if __name__ == "__main__":
     print(solve('puzzle.txt'))
+    print(solve2('puzzle.txt'))
--- 2025/11/d11_test.py
+++ 2025/11/d11_test.py
@@ -1,5 +1,6 @@
-from d11 import solve
+from d11 import solve, solve2
 
 
 def test_solves_example():
     assert solve('example.txt') == 5
+    assert solve2('example2.txt') == 2
